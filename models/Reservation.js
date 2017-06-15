34a5377e5fd762e31617a1b1e1843d5ba79ba358
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-06-15 22:41:42 +0200

    Started working on Reservation model

---
+++ models/Reservation.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Reservation = new Schema({
+  amountOfChairs: {type: Number, min: 1, max: 12, required: [true, "Enter the amount of chairs you want at the table."]},
+  nameOfReservation: {type: String, required: [true, "Enter the name of the person who made the reservation."]}
+});
