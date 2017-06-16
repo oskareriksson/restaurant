148655677bf166777e4b5dd270a9a0610270586b
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-06-16 18:14:00 +0200

    Added a basic User model which employees will use whilst making reservations

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const User = new Schema({
+  username: {type: String, required: [true, "A username is required to register/log in"]},
+  password: {type: String, required: [true, "A username is required to register/log in"]}
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", User);
