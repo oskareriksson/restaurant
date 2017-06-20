f3d6d1e6be7e10dbe5a0c72374d0e742ba3fa457
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-06-20 18:57:14 +0200

    Added a POST route in users.js so that you can add users to the database

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,16 @@ router.get("/register", (req, res) => {
   });
 });
 
+//Route for registering a new user
+router.post("/register", (req, res) => {
+  User.register(new User(
+    {
+      username: req.body.username,
+      password:req.body.password
+    }), req.body.password, (error, user) => {
+      if(error) res.send(error);
+      res.send("User successfully added!");
+    });
+});
+
 module.exports = router;
