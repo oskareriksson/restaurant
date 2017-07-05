47558a0b248e272b59e7e36b782f751a703cd30e
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-07-05 20:49:10 +0200

    Added a test that gets the "register" view

--- test/tests.js
+++ test/tests.js
@@ -7,11 +7,22 @@ chai.use(require("chai-things"));
 chai.use(chaiHttp);
 
 describe("General routes Tests", () => {
-  
+
   //This test gets the index view.
   it("Should get the index view", (done) => {
     chai.request(app)
       .get("/")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.have.header("content-type", "text/html; charset=utf-8");
+        done(err);
+      });
+  });
+
+  //This test gets the register view.
+  it("Should get the register view", (done) => {
+    chai.request(app)
+      .get("/users/register")
       .end((err, res) => {
         res.should.have.status(200);
         res.should.have.header("content-type", "text/html; charset=utf-8");
