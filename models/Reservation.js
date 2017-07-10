e99552da44476b54044b39883213a826f3795fac
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-07-10 20:54:22 +0200

    Forgot to export the actual model in Reservation.js before, fixed that now

--- models/Reservation.js
+++ models/Reservation.js
@@ -5,3 +5,5 @@ const Reservation = new Schema({
   amountOfChairs: {type: Number, min: 1, max: 12, required: [true, "Enter the amount of chairs you want at the table."]},
   nameOfReservation: {type: String, required: [true, "Enter the name of the person who made the reservation."]}
 });
+
+module.exports = mongoose.model("Reservation", Reservation);
